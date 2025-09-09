fb18092125ea6f89de92cc84ec78ffd4ba3c9b9c
Csongoranevem <czakocsongor@turr.hu>
2025-09-09 12:29:59 +0200

    frontend kapcsolas

--- server.js
+++ server.js
@@ -2,12 +2,21 @@ const express = require('express')
 
 const app = express()
 
-let users = 
-[
-    {id: 1, name: 'Béla', age: '18', gender: 'male'},
-    {id: 2, name: 'Lajos', age: '17', gender: 'male'},
-    {id:3, name: 'Héra', age: '22', gender: 'female'}
+const fs = require('fs')
+const path = require('path')
+var cors = require('cors')
+
+
+app.use(cors())
+app.use(express.json()) // json formátum megkövetelése
+app.use(express.urlencoded({extended: true})) // req body-n keresztül adatátmenet
+
+let users = [
+    {name: "Jonatan", 
+    email: "turr@turr.hu", 
+    password: "12234"}
 ]
+const USERS_FILE = path.join(__dirname, 'users.json')
 
 //Endpointok
 app.get('/', (req, res) => {
@@ -20,9 +29,96 @@ app.get('/users', (req, res) => {
   
   app.get('/user1', (req, res) => {
     let user1 = users.find(u => u.id == 1)
-    res.send("első: "+user1.name)
+    res.send(user1.name)
   })
   
+  app.post('/users', (req, res) => {
+    let data   = req.body
+    data.id = getNextId()
+    users.push(data)
+    if (EmailExists(data.email)) {
+      return res.status(400).send({msg: 'Létező fiók'})
+    }
+    res.send(users)
+    saveUser()
+  })
 
+  app.delete('/users/:id', (req, res) => {
+    let id = req.params.id
+    let idx = users.findIndex(user => user.id == id)
+    if (idx > -1) {
+      users.splice(idx, 1)
+      saveUser()
+      return res.send('Törölve')
+    }
+    return res.send('Nincs ilyen adat')
+
+    
+  })
+
+  app.patch('/users/:id', (req, res) => {
+    let id = req.params.id
+    let data   = req.body
+    let idx = users.findIndex(user => user.id == id)
+    if (idx > -1) {
+      users[idx] = data
+      users[idx].id = Number(id)
+      saveUser()
+      return res.send('Módosítva')
+    }
+    return res.send('Nincs ilyen adat')
+
+
+  })
+
+//res.status(200).send(body)
+
+//loadUsers()
 app.listen(3000)
 
+
+
+
+function getNextId() {
+  let maxIndex = 0
+  let nexID = 1
+    for (let i = 0; i < users.length; i++) {
+        if (users[i].id > users[maxIndex].id) {
+          maxIndex = i
+          nextID = users[maxIndex].id
+        }
+      }
+
+      return users[maxIndex].id + 1
+    }
+
+
+function loadUsers() {
+  if (fs.existsSync(USERS_FILE)) {
+    const raw = fs.readFileSync(USERS_FILE)
+    try {
+      users.JSON.parse(raw)
+    } catch (err) {
+      console.log('Hiba az adatok beolvasásában', err);
+      users = []
+    }
+  }
+  else{
+    saveUser()
+  }
+}
+
+function saveUser() {
+  fs.writeFileSync(USERS_FILE, JSON.stringify(users))
+}
+
+
+function EmailExists(email) {
+  let exists = false
+  users.forEach(user => {
+    if (user.email == email) {
+      exists = true
+      return
+    }
+  })
+}
