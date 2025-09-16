56270304fd7b269164e48db9d7e62677affec649
Csongoranevem <czakocsongor@turr.hu>
2025-09-16 13:20:31 +0200

    backend folytatás

--- server.js
+++ server.js
@@ -142,13 +142,46 @@ app.patch('/stepdataUpdate/:id', (req, res) => {
   if (idx > -1) {
     stepData[idx] = { ...stepData[idx], ...data, id: Number(id) 
   }
+    saveStepData()
     }
 })
 
+//GET all steps by userID
+app.get('/stepdataFill/:uid', (req, res)=> {
+  let userId = req.params.uid;
+  let idx = users.findIndex(user => user.id == userId);
+
+  if (idx == -1){
+      res.status(400).send({msg: 'Nincs ilyen felhasználó!'});
+      return
+  }
+
+  res.send(stepData.filter(step => step.userID == userId));
+})
 
 //DELETE step by ID
 
+app.delete('/stepdataDelete/:id', (req, res) => {
+  let id = req.params.id
+  let idx = stepData.findIndex(step => step.id == id)
+  if (idx > -1) {
+    stepData.splice(idx, 1)
+    saveStepData()
+    return res.send('Törölve')
+  }
+})
+
+
 //DELETE all steps by userID
+app.delete('/stepdataDeleteAll/:id', (req, res) => {
+  let userID = req.params.id
+  let idx = stepData.findIndex(step => step.userID == userID)
+  if (idx > -1) {
+    stepData = stepData.filter(step => step.userID != userID)
+    saveStepData()
+    return res.send('Törölve az összes adat')
+  }
+})
 
 
 
